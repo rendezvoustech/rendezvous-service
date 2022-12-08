91e05ef01f7d008ab844b3cc0e85bbc513588b5c
Mikkel Arentoft <mikkel@mikadocs.com>
2022-12-08 20:12:10 +0100

    Added logging and telemetry

--- src/main/java/tech/rendezvous/rendezvousservice/config/SecurityConfig.java
+++ src/main/java/tech/rendezvous/rendezvousservice/config/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig {
     SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
